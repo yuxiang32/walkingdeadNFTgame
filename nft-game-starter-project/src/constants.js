e392914e8990a4cb4cb941a12cb1ffd7b90f2870
Zheng Yuxiang <yuxiang32@gmail.com>
2021-12-04 02:57:31 +0800

    Added files necessary for web app

---
+++ nft-game-starter-project/src/constants.js
@@ -0,0 +1,16 @@
+const CONTRACT_ADDRESS = '0x5086Cd89E9Ec272A22B357b8122aD2dE548b5E0b';
+
+/*
+ * Add this method and make sure to export it on the bottom!
+ */
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
